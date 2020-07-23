1b2378779167e432bcef04fb180ab3ee54d1b1bc
Rachel D <dongb909@gmail.com>
2020-07-23 22:49:52 +0000

    First Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let express = require("express");
+let app = express();
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res)=>{
+	res.render("landing")
+});
+
+app.listen(3000, ()=>{
+	console.log("server has started!!")
+});
