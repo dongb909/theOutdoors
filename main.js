c945e5d0e5a142d9a47d33c078cb5a2accf01a76
Rachel D <dongb909@gmail.com>
2020-08-02 19:57:34 +0000

    Add basic seed data with cb hell version

--- main.js
+++ main.js
@@ -7,13 +7,6 @@ mongoose.connect("mongodb://localhost/the_outdoors", {useNewUrlParser: true, use
 app.use(bodyParser.urlencoded({extended:true}))
 app.set("view engine", "ejs")
 
-
-//"Yosemite National Park", "https://images.unsplash.com/photo-1518623380242-d992d3c57b37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1900&q=80"
-//"Zion ", "image" : "https://images.unsplash.com/photo-1533090631-338411005233?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
-//"Yoho", "https://images.unsplash.com/photo-1527489377706-5bf97e608852?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1654&q=80"
-//"Glacier", "image" : "https://images.unsplash.com/photo-1533496928027-041f8b02dc78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=932&q=80"
-
-
 app.get("/", (req, res)=>{
 	res.render("landing")
 });
@@ -23,7 +16,7 @@ app.get("/locations", (req, res)=>{
 		if (err){
 			console.log(err)
 		} else {
-			res.render("index", {locations: allLocations}); //sending data from db to index.ejs file
+			res.render("index", {locations: allLocations}); 
 		}
 	})
 });
