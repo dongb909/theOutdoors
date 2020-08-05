f0fdbbe65bf2f176f38a80a62cb6b760bf1dde73
Rachel D <dongb909@gmail.com>
2020-08-05 06:17:31 +0000

    Add comment form and show on page using async await instead of promises. Not to + location._id when redirecting because it needs to be a string

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ let express 	= require("express"),
 	Comment  	= require("./models/comment")
 	// seedDB		= require("./seeds")
 
+
+/*========================
+	CONNECTION
+==========================*/
 mongoose.connect("mongodb://localhost/the_outdoors", {useNewUrlParser: true, useUnifiedTopology: true}) 
 app.use(bodyParser.urlencoded({extended:true}))
 app.set("view engine", "ejs")
 
-
+/*========================
+	LOCATION ROUTES
+==========================*/
 app.get("/", (req, res)=>{
 	res.render("landing")
 });
@@ -20,7 +26,7 @@ app.get("/locations", (req, res)=>{
 		if (err){
 			console.log(err)
 		} else {
-			res.render("index", {locations: allLocations}); 
+			res.render("locations/index", {locations: allLocations}); 
 		}
 	})
 });
@@ -40,7 +46,7 @@ app.post("/locations", (req, res)=>{
 });
 
 app.get("/locations/add", (req, res)=>{
-	res.render("add")
+	res.render("locations/add")
 });
 
 //.populate adds the actual comments to our objs instead of the ids references
@@ -50,9 +56,28 @@ app.get("/locations/:id", (req, res)=>{
 		if (err) console.log(err);
 		else {
 			// console.log(foundLocation)
-			res.render("show", {location: foundLocation});
+			res.render("locations/show", {location: foundLocation});
 		}
 	})
+})
+
+/*========================
+	COMMENT ROUTES
+==========================*/
+
+app.get("/locations/:id/comments/add", (req, res) =>{
+	Location.findById(req.params.id, (err, foundLocation) => {
+		if (err) console.log(err);
+		else res.render("comments/add", {location: foundLocation});
+	});
+})
+
+app.post("/locations/:id/comments", async function (req, res){
+	let location = await Location.findById(req.params.id);
+	let newComment = await Comment.create({author: req.body.comment.author, text: req.body.comment.text});
+	location.comments.push(newComment);
+	location.save();	//do not do .populate here, you're using async await, NOT promises this way
+	res.redirect("/locations/" + location._id);	//NO HAVE TO PUT IN THE ACTUAL ID, ":ID" DOES NOT WORK because this is treated as a STRING, so :id will not convert to a number
 })
 
 app.listen(3000, ()=>{
