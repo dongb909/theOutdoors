127b1b78f8581a08a7a9d6dbe5d6c0ea2e2a9906
dongb909 <dongb909@gmail.com>
2021-01-20 17:31:37 -0800

    Change server port

--- main.js
+++ main.js
@@ -1,60 +1,64 @@
-require('dotenv').config()
-let express 	= require("express"),
-	app     	= express(),
-	bodyParser 	= require("body-parser"),
-	mongoose 	= require("mongoose"),
-	flash		= require("connect-flash"),
-	Location 	= require("./models/location"),
-	Comment  	= require("./models/comment"),
-	methodOverride = require("method-override"),
-	passport	= require("passport"),
-	LocalStrategy = require("passport-local"),
-	User 		= require("./models/user"),
-	expressSession = require("express-session"),
-	seedDB		= require("./seeds"),
-	PW 			= process.env.PW
-require('dotenv').config()
-
+require("dotenv").config();
+let express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  Location = require("./models/location"),
+  Comment = require("./models/comment"),
+  methodOverride = require("method-override"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  User = require("./models/user"),
+  expressSession = require("express-session"),
+  seedDB = require("./seeds"),
+  PW = process.env.PW;
+require("dotenv").config();
 
 // seedDB();
 /*========================
 	REQUIRING ROUTES
 ==========================*/
 let commentRoutes = require("./routes/comments"),
-	locationRoutes = require("./routes/locations"),
-	indexRoutes = require("./routes/index")
-
+  locationRoutes = require("./routes/locations"),
+  indexRoutes = require("./routes/index");
 
 /*========================
 	CONNECTION
 ==========================*/
-mongoose.connect(`mongodb+srv://dongb909:${PW}@theoutdoors.cnl85.mongodb.net/TheOutdoors?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true}); 
-app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect(
+  `mongodb+srv://dongb909:${PW}@theoutdoors.cnl85.mongodb.net/TheOutdoors?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(flash());
 
 /*========================
 	PASSPORT CONFIGURATION
 ==========================*/
-app.use(expressSession({
-	secret: "Hi my name is Rachel!",
-	resave: false,
-	saveUninitialized: false
-}));
-app.use(passport.initialize()); 
+app.use(
+  expressSession({
+    secret: "Hi my name is Rachel!",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function (req,res,next){	//a middleware that runs for EVERY route thus every route and template has access to the variable "currentUser"
-	res.locals.currentUser = req.user; //only avail if user is loggedin or else it'll return undefined
-	res.locals.flashMessageError = req.flash("flashMessageError");
-	res.locals.flashMessageSuccess = req.flash("flashMessageSuccess");
-	//currentUser = {_id: req.user.id}
-	next();
+app.use(function (req, res, next) {
+  //a middleware that runs for EVERY route thus every route and template has access to the variable "currentUser"
+  res.locals.currentUser = req.user; //only avail if user is loggedin or else it'll return undefined
+  res.locals.flashMessageError = req.flash("flashMessageError");
+  res.locals.flashMessageSuccess = req.flash("flashMessageSuccess");
+  //currentUser = {_id: req.user.id}
+  next();
 });
 
 /*========================
@@ -62,9 +66,8 @@ app.use(function (req,res,next){	//a middleware that runs for EVERY route thus e
 ==========================*/
 app.use(indexRoutes);
 app.use("/locations/:id/comments", commentRoutes);
-app.use("/locations", locationRoutes); 
+app.use("/locations", locationRoutes);
 
-
-app.listen(3000, ()=>{
-	console.log("server has started!!")
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server has started!!");
 });
